cc542dba23e07dec7a177bc06bb4d9e9e3556779
Nicolas Abbal <nicolas.abbal@gmail.com>
2019-05-18 17:01:26 +0200

    Backported WS fixes.

--- ws-handlers/protected.js
+++ ws-handlers/protected.js
@@ -3,7 +3,7 @@ const logger = require('../lib/logger');
 function addProtectedHandlers(socket) {
   const ip = socket.request.connection.remoteAddress;
   socket.on('testProtected', (data) => {
-    logger.info(`Web socket [protected]: received testProtected from ${ip}: `, data);
+    logger.info(`Web socket [protected]: received testProtected from user ${socket.decodedToken.username} on ${ip}: `, data);
   });
 }
 
