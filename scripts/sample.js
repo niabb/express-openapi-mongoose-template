3eb244ffdd47ef1f06ff56040520536f0d3ad6d0
Nicolas Abbal <nicolas.abbal@gmail.com>
2020-11-30 20:15:07 +0100

    Fixed sample script

--- scripts/sample.js
+++ scripts/sample.js
@@ -2,6 +2,7 @@
  * The purpose of the scripts folder is to provide a way to run standalone scripts on the back-end
  * side (i.e. periodically), to call methods at the service level.
  */
+require('dotenv').config();
 
 const logger = require('../lib/logger');
 const userService = require('../services/user');
